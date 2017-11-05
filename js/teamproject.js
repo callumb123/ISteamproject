59d9863a4b23bb3e2f6f017cab9749a532b24303
Callum Boyd <c-r-boyd@hotmail.com>
2017-11-05 22:11:16 +0000

    Plotted markers on map

--- js/teamproject.js
+++ js/teamproject.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-  var locations = []
+function initMap(){
+  var uluru = {lat: 46.003257147967815399169921875, lng: 8.95168307237327098846435546875};
+  var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 14,
+		center: uluru
+		});
   $.ajax({
     url: "xml/running.gpx",
     dataType: "xml",
@@ -9,35 +13,14 @@ $(document).ready(function(){
 	$(data).find('trk trkseg trkpt').each(function() {
 		var currentLat = $(this).attr("lat");
 		var currentLon = $(this).attr("lon");
-		locations.push({latitude: currentLat,
-                  longitude: currentLon
-                  });
+		var point = {lat: parseFloat(currentLat), lng: parseFloat(currentLon)};
+		var marker = new google.maps.Marker({
+				position: point,
+				map: map
+				});
+		marker.setMap(map);
 		var currentElevation = $(this).find('ele').text();
 		var currentTime = $(this).find('time').text();
-
-		$('#lat ul').append(
-			$('<li />', {
-				text: currentLat
-			})
-		);
-
-		$('#lon ul').append(
-			$('<li />', {
-				text: currentLon
-			})
-		);
-
-		$('#elevation ul').append(
-			$('<li />', {
-				text: currentElevation
-			})
-		);
-
-		$('#time ul').append(
-			$('<li />', {
-				text: currentTime
-			})
-		);
 	});
     },
     error: function(){
@@ -46,7 +29,7 @@ $(document).ready(function(){
 
   });
 
-});
+};
 
 
 
